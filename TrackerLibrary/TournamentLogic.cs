340dd56a9aa4049c059b9d5c157b010dda5afa07
AkankshaAgg <40016511+AkankshaAgg@users.noreply.github.com>
2020-12-16 15:23:07 +0530

    Generating Email Logic

--- TrackerLibrary/TournamentLogic.cs
+++ TrackerLibrary/TournamentLogic.cs
@@ -29,8 +29,8 @@ namespace TrackerLibrary
 
         public static void updateTournamentResults(TournamentModel model)
         {
+            int startingRound = model.CheckCurrentRound();
             
-            //GlobalConfig.Connection.UpdateMatchup(m);
             List<MatchupModel> toScore = new List<MatchupModel>();
 
             foreach(List<MatchupModel> round in model.Rounds)
@@ -49,6 +49,72 @@ namespace TrackerLibrary
             AdvanceWinners(toScore, model);
 
             toScore.ForEach(x => GlobalConfig.Connection.UpdateMatchup(x));
+
+            int endingRound = model.CheckCurrentRound();
+
+            if(endingRound > startingRound)
+            {
+                //Alert users
+                //EmailLogic.SendEmail()
+            }
+        }
+
+        private static void AlertUsersToNewRound(this TournamentModel model)
+        {
+            int currentRoundNumber = model.CheckCurrentRound();
+            List<MatchupModel> currentRound = model.Rounds.Where(x => x.First().MatchupRound == currentRoundNumber).First();
+
+            foreach (MatchupModel matchup in currentRound)
+            {
+                foreach (MatchupEntryModel me in matchup.Entries)
+                {
+                    foreach (PersonModel p in me.TeamCompeting.TeamMembers)
+                    {
+                        AlertPersonToNewRound(p, me.TeamCompeting.TeamName, matchup.Entries.Where(x => x.TeamCompeting != me.TeamCompeting).FirstOrDefault());
+                    }
+                }
+            }
+        }
+
+        private static void AlertPersonToNewRound(PersonModel p, string teamName, MatchupEntryModel competitor)
+        {
+            string from = "";
+            List<string> to = new List<string>();
+            string subject = "";
+            string body = "";
+            //stringbuilder to concatenate strings
+            StringBuilder sb = new StringBuilder();
+
+            if(competitor != null)
+            {
+                subject = $"You have a new matchup with { competitor.TeamCompeting.TeamName }";
+            }
+            else
+            {
+                subject = "You have a bye week this round.";
+            }
+
+            sb.AppendLine("<h1>You have a new matchup</h1>");
+            sb.AppendLine("<strong>Competitor: </strong>");
+            sb.Append(competitor.TeamCompeting.TeamName);
+            sb.AppendLine();
+            sb.AppendLine();
+            sb.AppendLine("Have a great time!");
+
+            EmailLogic.SendEmail(from, to, subject, body);
+        }
+
+        private static int CheckCurrentRound(this TournamentModel model)
+        {
+            int output = 1;
+            foreach(List<MatchupModel> round in model.Rounds)
+            {
+                if (round.All(x => x.Winner != null))
+                {
+                    output += 1;
+                }
+            }
+            return output;
         }
 
         private static void AdvanceWinners(List<MatchupModel> models, TournamentModel tournament)
